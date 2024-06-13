bb09f5f9c079da3ac74993d5f4ce7e6f6a95b82b
Charly1987fc <chajo87@gmail.com>
2024-06-13 17:39:40 -0600

    Add files via upload

---
+++ Examen1Progra2App/Program.cs
@@ -0,0 +1,43 @@
+﻿using Examen1Progra2App.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1Progra2App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int op = 0;
+            clsClientes cli = new clsClientes();
+            
+            while (op != 6)
+            {
+                Console.WriteLine("Bienvenido a Autosoluciones");
+                Console.WriteLine("Favor elija una opcion");
+                Console.WriteLine("1.Gestion de clientes");  //Esta opcion escogi "Gestion de clientes"
+                Console.WriteLine("2.Gestion de vehiculos");
+                Console.WriteLine("3.Gestion de servicios");
+                Console.WriteLine("4.Gestion de citas");
+                Console.WriteLine("5.Gestion de mecanicos");
+                Console.WriteLine("6.Salir");
+               
+                op = int.Parse(Console.ReadLine());
+                switch (op) 
+                {
+                    case 1:
+                        
+                            cli.cli();
+                        
+                        
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Examen1Progra2App/Properties/clsClientes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1Progra2App.Properties
+{
+    internal class clsClientes
+    {
+        public void cli()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Ingrese el nombre del cliente:");
+                string nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el telefono del cliente:");
+                int telefono = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el correo del cliente:");
+                string correo = Console.ReadLine();
+
+                Console.WriteLine("Ingrese la direccion del cliente:");
+                string direccion = Console.ReadLine();
+
+                Cliente nuevoCliente = new Cliente(nombre, correo, direccion, telefono);
+                clientes.Add(nuevoCliente);
+
+                Console.WriteLine("¿Desea agregar otro cliente? (si/no)");
+                string respuesta = Console.ReadLine();
+                continuar = respuesta.ToLower() == "si";
+            }
+
+            Console.WriteLine("Lista de clientes inscritos:");
+            foreach (Cliente cliente in clientes)
+            {
+                Console.WriteLine($"Nombre: {cliente.Nombre}, Telefono: {cliente.Telefono}, Correo: {cliente.Correo}, Direccion: {cliente.Direccion}");
+            }
+        }
+
+        
+
+        class Cliente
+        {
+            public string Nombre { get; set; }
+            public string Correo { get; set; }
+            public int Telefono { get; set; }
+            public string Direccion { get; set; }
+
+            public Cliente(string nombre, string correo, string direccion, int telefono)
+            {
+                Nombre = nombre;
+                Telefono = telefono;
+                Correo= correo;
+                Direccion=direccion;
+            }
+        }
+    }
+}
+
